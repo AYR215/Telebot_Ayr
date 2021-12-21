3a66b689b27f27b1bd628a0929939645d0c96e7e
AYR215 <90398255+AYR215@users.noreply.github.com>
2021-12-21 14:49:00 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5076070620:AAGgZdDnC3T2gTzXkaMckZ4unjwagxUF5hc"
+
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'евро': 'EUR',
+        'рубль': 'RUB'
+}
